41b8a075485bd7eaa3b6912d179229fecf175f05
Abdul <abdul.mouhamadsultane@exensa.com>
2016-07-01 11:55:45 +0200

    setup_new_slaves modified to avoid formatting the namenode

--- ephemeral-hdfs/setup_new_slaves.sh
+++ ephemeral-hdfs/setup_new_slaves.sh
@@ -17,15 +17,6 @@ wait
 
 /root/spark-ec2/copy-dir $EPHEMERAL_HDFS/conf
 
-NAMENODE_DIR=/mnt/ephemeral-hdfs/dfs/name
-
-if [ -f "$NAMENODE_DIR/current/VERSION" ] && [ -f "$NAMENODE_DIR/current/fsimage" ]; then
-  echo "Hadoop namenode appears to be formatted: skipping"
-else
-  echo "Formatting ephemeral HDFS namenode..."
-  $EPHEMERAL_HDFS/bin/hadoop namenode -format
-fi
-
 echo "Starting ephemeral HDFS..."
 
 # This is different depending on version.
